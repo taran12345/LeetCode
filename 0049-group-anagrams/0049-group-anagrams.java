15c9593e04de1a9bd65384124189c330095ccf22
Taran gangil <40990378+tarangangil@users.noreply.github.com>
2023-11-19 14:06:18 +0530

    Time: 6 ms (73.90%), Space: 47.4 MB (19.17%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+    Map<String, List<String>> map = new HashMap<>();
+
+    for (final String str : strs) {
+      char[] chars = str.toCharArray();
+      Arrays.sort(chars);
+      String key = String.valueOf(chars);
+      map.computeIfAbsent(key, k -> new ArrayList<>()).add(str);
+    }
+
+    return new ArrayList<>(map.values());
+    }
+}
